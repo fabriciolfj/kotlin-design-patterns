d40471e4d034c26ed7392ff08c956b3c9fbffc49
fabricio <fabricio.jacob@outlook.com>
2022-02-01 23:47:00 -0300

    add

--- src/main/kotlin/com/github/fabricio/design/decorator/LoggingGetCaptainStarTrekRepository.kt
+++ src/main/kotlin/com/github/fabricio/design/decorator/LoggingGetCaptainStarTrekRepository.kt
@@ -1,5 +1,6 @@
 package com.github.fabricio.design.decorator
 
+//by delega a implementação para outro objeto, no caso se omitirmos o getCaptaian, ele pegaria de outra classe que implementa tal interface
 class LoggingGetCaptainStarTrekRepository(private val repository: StarTrekRepository) : StarTrekRepository by repository {
 
     override fun getCaptain(starship: String): String {
